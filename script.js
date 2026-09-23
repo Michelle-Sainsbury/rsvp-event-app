ed9c0333d8211e2978d07ae62d6a241faba5ceeb
msainsburrrrrr45-spec <msainsbury45@gmail.com>
2026-09-23 13:57:39 -0400

    Add attendee registration form and submission flow

--- script.js
+++ script.js
@@ -15,3 +15,32 @@ app.innerHTML = `
     <button id="registerBtn">Register for Event</button>
   </section>
 `;
+const registerBtn = document.getElementById("registerBtn");
+
+registerBtn.addEventListener("click", () => {
+  app.innerHTML = `
+    <section class="registration-form">
+      <h2>Register for Community Tech Night</h2>
+
+      <form id="registrationForm">
+        <label for="name">Full Name</label>
+        <input type="text" id="name" required>
+
+        <label for="email">Email Address</label>
+        <input type="email" id="email" required>
+
+        <button type="submit">Complete Registration</button>
+      </form>
+    </section>
+  `;
+  const registrationForm = document.getElementById("registrationForm");
+
+registrationForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  
+  alert(`Registration complete for ${name}!`);
+});
+});
